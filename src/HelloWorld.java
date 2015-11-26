c5ef5696f10f365aa8af014e8fde1dc098e11493
jorge <jorgearteiro@hotmail.com>
2015-11-26 23:08:52 +0000

    GitHub CI test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ c class HelloWorld
 {
         public static void main (String [] args)
         {
-                System.out.println("hello world");
+                System.out.println("hello world GitHub test");
         }
 }
 
